55023a75a495e307e8e61e3432d92c0115fcee0e
NamanBhat <46017087+NamanBhat@users.noreply.github.com>
2024-03-30 04:46:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = {
+        name: 'Naman Bhat',
+        skills: ['JavaScript', 'React', 'Node', 'Python', 'Java', 'C++', 'C', 'HTML', 'CSS', 'SQL', 'MongoDB' ]
+    };
+    return member;
+}
